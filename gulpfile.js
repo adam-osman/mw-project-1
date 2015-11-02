bd3b58196cf1473b7b755bfb34dc8c58c89c6304
adam-osman <osman1ak@yahoo.com>
2015-11-02 18:51:17 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require ('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('holla', function (){
-  console.log('YO');
+gulp.task('sass', function (){
+  return gulp.src('scss/style.css')
+  .pipe(sass())
+  .pipe(gulp.dest('assets/css'));
 });
